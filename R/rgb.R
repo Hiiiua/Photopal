89dacb869160b925c2d7db4fdf6b308cb5fd40cc
Hiiiua <mhua4816@gmail.com>
2023-03-26 17:55:32 -0500

    structure of Photopal package

---
+++ R/rgb.R
@@ -0,0 +1,13 @@
+#' A testing function
+#' 
+#' @description 
+#' `test_function()` prints input number. 
+#' This is just a testing function to make sure the package structure works.
+#' 
+#' @param x A number.
+#' @export
+#' @examples 
+#' test_function(1)
+test_function <- function(x){
+  print(x)
+}

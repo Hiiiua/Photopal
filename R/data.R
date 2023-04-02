62c647661f3e918ad478f7bdd34098c21a183f3e
Hiiiua <mhua4816@gmail.com>
2023-04-02 13:36:01 -0500

    start making initial code into functions

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' A Nebraska football stadium image data
+#' 
+#' An 4 dimensional image data
+#' 
+#' @format ## `image`
+#' A data of 4 dimensions, with 1500 rows, 1000 columns, 1 depth and 3 color channels.
+#' @source <https://topbrunchspots.com/wp-content/uploads/2022/05/AF1QipM-ql78yz46DGynt2tb1l7i8gK8zOAfN8dCQqiPw1600-h1000-k-no.jpeg>
+"image"
--- R/rgb.R
+++ R/rgb.R
@@ -11,3 +11,26 @@
 test_function <- function(x){
   print(x)
 }
+
+
+#' A function read in an image from url, locally or default build-in image.
+#' 
+#' @param url A string. 
+#' @importFrom imager load.image
+#' @export
+image2rgb <- function(url = paste('https://topbrunchspots.com', 
+                                  '/wp-content/uploads/2022/05/AF1QipM',
+                                  '-ql78yz46DGynt2tb1l7i8gK8zOAfN8dCQqiP',
+                                  'w1600-h1000-k-no.jpeg', sep='')){
+  if (length(url) > 0 ){
+    image = imager::load.image(url)
+  }
+  else{
+    image = load('../data/image.rda')
+  }
+  rgb_df = data.frame(red = as.vector(image[,,,1]),
+                      green = as.vector(image[,,,2]),
+                      blue = as.vector(image[,,,3]))
+  return(rgb_df)
+}
+

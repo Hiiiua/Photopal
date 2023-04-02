b4e0c3e8b40fa0ca4fae379c3ed89b211c63a155
Hiiiua <mhua4816@gmail.com>
2023-04-02 14:24:41 -0500

    Remove test_function

--- R/rgb.R
+++ R/rgb.R
@@ -1,18 +1,3 @@
-#' A testing function
-#' 
-#' @description 
-#' `test_function()` prints input number. 
-#' This is just a testing function to make sure the package structure works.
-#' 
-#' @param x A number.
-#' @export
-#' @examples 
-#' test_function(1)
-test_function <- function(x){
-  print(x)
-}
-
-
 #' A function read in an image from url, locally or default build-in image.
 #' 
 #' @param url A string. 

394c8039d5828f038f0084e500cbe51c43afa868
Hiiiua <mhua4816@gmail.com>
2023-04-08 20:22:52 -0500

    Replace the data from resized copyrighted image

--- R/data.R
+++ R/data.R
@@ -2,7 +2,7 @@
 #' 
 #' An 4 dimensional image data
 #' 
-#' @format ## `image`
-#' A data of 4 dimensions, with 1500 rows, 1000 columns, 1 depth and 3 color channels.
-#' @source <https://topbrunchspots.com/wp-content/uploads/2022/05/AF1QipM-ql78yz46DGynt2tb1l7i8gK8zOAfN8dCQqiPw1600-h1000-k-no.jpeg>
-"image"
+#' @format
+#' A data of 4 dimensions, with 1806 rows, 605 columns, 1 depth and 3 color channels.
+#' @source <https://github.com/TWiedRW>
+"stadium"
--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -1,14 +1,47 @@
-url_invalid = "df"
-valid = 'https://topbrunchspots.com/wp-content/uploads/2022/05/AF1QipM-ql78yz46DGynt2tb1l7i8gK8zOAfN8dCQqiPw1600-h1000-k-no.jpeg'
+loc_invalid = "invalid"
+valid = paste('https://topbrunchspots.com', 
+              '/wp-content/uploads/2022/05/AF1QipM',
+              '-ql78yz46DGynt2tb1l7i8gK8zOAfN8dCQqiP',
+              'w1600-h1000-k-no.jpeg', sep='')
 
-test_that("load image from url works", {
-  # raise error when url is not valid
-  expect_error(image2rgb(url_invalid)) 
+test_that("if loading an image from a url or a location works", {
+  # throw error when url or location is not valid
+  expect_error(image2rgb(loc_invalid))
   
   # built-in data returns a dataframe
-  expect_true(class(image2rgb()) == 'data.frame')
+  expect_true(class(image2rgb()) == "data.frame")
   
   # output dataframe has three color channels
   expect_equal(dim(image2rgb())[2], 3)  # built-in image file
   expect_equal(dim(image2rgb(valid))[2], 3) # image file from valid url
 })
+
+
+
+# c1, c2 colors should has delta e distance around 35
+c1_1 = c(0.2745098, 0.1960784, 0.1960784)
+c3_1 = c(0.1960784, 0.1960784, 0.1960784)
+c1 = c(70, 50, 50)
+c2 = c(0, 0, 0)
+c3 = c(50, 50, 50) 
+
+test_that("How many contrast", {
+  # color distance match
+  expect_equal(round(contrast(c1,c2)), 25)
+  expect_equal(round(contrast(c2, c3)), 21)
+  
+  # should also works with max color value 1
+  expect_equal(round(contrast(c1,c2)), round(contrast(c1_1,c2, maxColorValue = 1)))
+  expect_equal(round(contrast(c2,c3)), round(contrast(c2,c3_1, maxColorValue = 1)))
+})
+
+
+test_that("Is contrast sufficient", {
+  # c1 and c2 should match 25
+  expect_true(is_sufficient(c1,c2, threshold = 25))
+  expect_false(is_sufficient(c1, c2, threshold = 35))
+})
+
+
+
+

55acc518d901805105992aa523c0fc3899131ba9
TWiedRW <twiederich@icloud.com>
2023-05-02 21:34:34 -0500

    Added full setup for "Create a palette"

--- R/showcaseApp.R
+++ R/showcaseApp.R
@@ -14,129 +14,152 @@ showcaseApp <- function(){
 } 
 
 
-# showcaseApp <- function(){
-# # UI ----------------------------------------------------------------------
-# 
-#   
-#   #Color palette page
-#   createPalettePage <- fluidPage(
-#     useShinyjs(),
-#     sidebarLayout(
-#       sidebarPanel(
-#         h2('Instructions'),
-#         p("Welcome to the Photopal showcase app! To get started, either upload an image with a valid extension (.png or .jpeg) or use our app's default image."),
-#         fileInput('userPic', 'Upload file'),
-#         uiOutput('clearUserPic'),
-#         actionButton('useDefault', 'Use default image')
-#         
-#       ), #end sidebar panel
-#       mainPanel(
-#         
-#         fluidRow(
-#           column(width = 6, align = 'center',
-#             p('This is where the palette will go'),
-#             verbatimTextOutput('test')
-#           ),
-#           column(width = 6, align = 'center',
-#                  p('This is where the input image will go')
-#             
-#           )
-#         )
-#         
-#       ) #end main panel
-#     ) #end sidebar layout
-#   ) #end createPalettePage
-#   
-#   
-#   
-#   
-#   
-#   #Colorblind page
-#   colorblindPage <- fluidPage(
-#     
-#   ) #end colorblind page
-#   
-#   
-#   #Color analysis
-#   colorAnalysisPage <- fluidPage(
-#     
-#   ) #end color analysis page
-#   
-#   
-#   
-#   #Main page layout
-#   ui <- navbarPage("Photopal", theme = shinytheme('flatly'),
-#                    tabPanel("Create a palette", createPalettePage),
-#                    tabPanel("Colorblind", colorblindPage),
-#                    tabPanel("Image color analysis", colorAnalysisPage)
-#   )
-#   
-#   
-#   
-#   
-# 
-# # Server ------------------------------------------------------------------
-# 
-#   server <- function(input, output, session){
-#     
-#     
-#     output$test <- renderPrint(appData$userPhoto) #See file input
-#     
-#     
-#     #Data inputted from user
-#     appData <- reactiveValues(
-#       userPhoto = NULL
-#     )#end
-#     
-#     
-#     
-#     #Assign uploaded pic to reactive values
-#     observeEvent(input$userPic, { 
-#       appData$userPhoto <- input$userPic
-#     })#end
-#     
-#     
-#     
-#     
-#     #Shows button to clear uploaded file only if a file is present
-#     output$clearUserPic <- renderUI({ 
-#       if(!is.null(appData$userPhoto)){
-#         list(actionButton('clearPic', 'Clear uploaded image'),
-#              p(' '))
-#       }
-#     })#end
-#     
-#     
-#     
-#     
-#     #Reset file and data
-#     observeEvent(input$clearPic, { 
-#       reset('userPic')
-#       appData$userPhoto <- NULL
-#     })
-#     observeEvent(input$useDefault, { 
-#       reset('userPic')
-#       appData$userPhoto <- NULL
-#     })#end
-#     
-#     
-#   
-#     
-#     
-#     
-#     
-#     
-#   } #end server
-#   
-#   
-#   shinyApp(ui, server) #run app
-#   
-#   
-# } #end function
-# 
-# showcaseApp()
-# 
-# 
-# 
-# 
-# 
+
+
+library(shiny)
+library(shinyjs)
+library(Photopal)
+library(shinythemes)
+library(imager)
+library(stringr)
+
+
+showcaseApp <- function(){
+# UI ----------------------------------------------------------------------
+
+
+  #Color palette page
+  createPalettePage <- fluidPage(
+    sidebarLayout(
+      sidebarPanel(
+        h2('Instructions'),
+        p("Welcome to the Photopal showcase app! To get started, either upload an image with a valid extension (.png or .jpeg) or use our app's default image."),
+        fileInput('userPic', 'Upload file',
+                  accept = c('.png', '.jpeg')),
+        actionButton('useDefault', 'Use default image'),
+        h2(' '),
+        numericInput('numcols', 'How many colors for palette?', value = 5, min = 1, step = 1),
+        actionButton('update', 'Update number of colors')
+
+      ), #end sidebar panel
+      mainPanel(
+
+        fluidRow(
+          column(width = 4, align = 'center',
+            # p('This is where the palette will go'),
+            shinycssloaders::withSpinner(plotOutput('palettePlot')),
+            shinycssloaders::withSpinner(tableOutput('palette'))
+          ),
+          column(width = 8, align = 'center',
+                 # p('This is where the input image will go'),
+                 shinycssloaders::withSpinner(plotOutput('photo'))
+
+          )
+        )
+
+      ) #end main panel
+    ) #end sidebar layout
+  ) #end createPalettePage
+
+
+
+
+
+  #Colorblind page
+  colorblindPage <- fluidPage(
+
+  ) #end colorblind page
+
+
+  #Color analysis
+  colorAnalysisPage <- fluidPage(
+
+  ) #end color analysis page
+
+
+
+  #Main page layout
+  ui <- navbarPage("Photopal", theme = shinytheme('flatly'),
+                   tabPanel("Create a palette", createPalettePage),
+                   tabPanel("Colorblind", colorblindPage),
+                   tabPanel("Image color analysis", colorAnalysisPage)
+  )
+
+
+
+
+
+# Server ------------------------------------------------------------------
+
+  server <- function(input, output, session){
+
+    #Data inputted from user
+    appData <- reactiveValues(
+      userPhoto = NULL,
+      defaultPhoto = stadium,
+      palPhoto = NULL,
+      pal = NULL
+    )#end
+    
+    #Observe new file upload from user
+    observeEvent(input$userPic, {
+      appData$userPhoto <- input$userPic
+      validate(need(!is.null(appData$userPhoto), 'Photo needs to be uploaded'))
+      appData$palPhoto <- load.image(appData$userPhoto[['datapath']])
+      appData$pal <- palette_create(input$numcols, image2rgb(appData$userPhoto[['datapath']]), proceed = T)
+    })
+
+    #Observe default button pushed
+    observeEvent(input$useDefault, {
+      appData$palPhoto <- appData$defaultPhoto
+      appData$pal <- palette_create(input$numcols, image2rgb(), proceed = T)
+    })
+    
+    #Update number of colors
+    observeEvent(input$update, {
+      validate(need(!is.null(appData$palPhoto), ' '))
+      appData$pal <- palette_create(input$numcols, image2rgb(), proceed = T)
+    })
+    
+
+    #Plot palette colors
+    output$palettePlot <- renderPlot({
+      validate(need(!is.null(appData$pal), ' '))
+      barplot(sort(table(appData$pal[,'palColors'])), col = sort(appData$pal[,'palColors']), axes = F,
+              xaxt="n", yaxt="n")
+    })
+    
+    #Plot palette color names
+    output$palette <- renderTable({
+      validate(need(!is.null(appData$pal), ' '))
+      pal = data.frame(appData$pal[,'palColors'])
+      names(pal) <- c('Palette Colors')
+      pal
+    })
+
+    #Plot photo
+    output$photo <- renderPlot({
+      validate(need(!is.null(appData$palPhoto), ' '))
+      plot(appData$palPhoto, axes = F)
+    })
+
+
+
+
+
+
+
+
+
+  } #end server
+
+  shinyApp(ui, server) #run app
+
+} #end function
+
+showcaseApp()
+
+
+
+
+
